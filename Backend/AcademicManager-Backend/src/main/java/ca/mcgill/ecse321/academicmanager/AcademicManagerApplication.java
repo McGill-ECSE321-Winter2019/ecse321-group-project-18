4a6e4275a828ff613a8e5936e10a98067c06adbf
Edward Huang <edhuang555@gmail.com>
2019-02-03 14:17:32 -0500

    Initial Spring Boot project setup

---
+++ Backend/AcademicManager-Backend/src/main/java/ca/mcgill/ecse321/academicmanager/AcademicManagerApplication.java
@@ -0,0 +1,14 @@
+package ca.mcgill.ecse321.academicmanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AcademicManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AcademicManagerApplication.class, args);
+	}
+
+}
+
