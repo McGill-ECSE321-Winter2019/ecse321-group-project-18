c6d9beb2b790f842db74a96bfac64990cedde4cd
Moetassem Abdelazim <moetassem.abdelazim@mail.mcgill.ca>
2019-02-15 17:18:02 -0500

    Added the controller and dto packages with the studentDto creation

---
+++ Backend/src/main/java/ca/mcgill/ecse321/academicmanager/controller/AcademicManagerRestController.java
@@ -0,0 +1,36 @@
+package ca.mcgill.ecse321.academicmanager.controller;
+import ca.mcgill.ecse321.academicmanager.dto.*;
+import ca.mcgill.ecse321.academicmanager.model.*;
+import ca.mcgill.ecse321.academicmanager.service.*;
+
+import javax.xml.ws.Service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@CrossOrigin(origins = "*")
+@RestController
+public class AcademicManagerRestController {
+	@Autowired
+	AcademicManagerService service;
+	@PostMapping(value = { "/Students/{name}", "/Students/{name}/" })
+	public StudentDto createStudent(@PathVariable("studentID") String studentID, 
+									@PathVariable("firstName") String firstName,
+									@PathVariable("lastName") String lastName) throws IllegalArgumentException {
+		// @formatter:on
+		Cooperator coop = service.createCooperator(1);
+		Student student = service.createStudent(studentID, firstName, lastName, coop);
+		return convertToDto(student);
+	}
+	
+	private StudentDto convertToDto(Student e) {
+		if (e == null) {
+			throw new IllegalArgumentException("There student doens't exist in this Cooperator!");
+		}
+		StudentDto studentDto = new StudentDto(e.getStudentID(),e.getFirstName(),e.getLastName());
+		return studentDto;
+	}
+}
---
+++ Backend/src/main/java/ca/mcgill/ecse321/academicmanager/dto/StudentDto.java
@@ -0,0 +1,39 @@
+package ca.mcgill.ecse321.academicmanager.dto;
+
+public class StudentDto {
+
+	private String studentID;
+	private String firstName;
+	private String lastName;
+	private boolean isProblematic;
+	
+	public StudentDto() {
+	}
+	
+	public StudentDto(String studentID, String firstName, String lastName) {
+		this(studentID, firstName, lastName, false);
+	}
+
+	public StudentDto(String studentID, String firstName, String lastName, boolean isProblematic) {
+		this.studentID = studentID;
+		this.firstName = firstName;
+		this.lastName = lastName;
+		this.isProblematic = isProblematic;
+	}
+	
+	public String getStudentID() {
+		return studentID;
+	}
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public boolean getStudentProblematicStatus() {
+		return isProblematic;
+	}
+}
