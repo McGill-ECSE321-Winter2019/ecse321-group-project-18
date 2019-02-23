244798c74463fe3ee1c2157b5a041066ef141bd7
Moetassem Abdelazim <moetassem.abdelazim@mail.mcgill.ca>
2019-02-23 17:12:53 -0500

    Added file coopTermRegistrationDto #25

---
+++ Backend/src/main/java/ca/mcgill/ecse321/academicmanager/dto/CoopTermRegistrationDto.java
@@ -0,0 +1,36 @@
+package ca.mcgill.ecse321.academicmanager.dto;
+
+public class CoopTermRegistrationDto {
+	
+	
+	
+	public CoopTermRegistrationDto() {
+	}
+	
+//	public CoopTermRegistrationDto(String studentID, String firstName, String lastName) {
+//		this(studentID, firstName, lastName, false);
+//	}
+
+//	public CoopTermRegistrationDto(String studentID, String firstName, String lastName, boolean isProblematic) {
+//		this.studentID = studentID;
+//		this.firstName = firstName;
+//		this.lastName = lastName;
+//		this.isProblematic = isProblematic;
+//	}
+//	
+//	public String getStudentID() {
+//		return studentID;
+//	}
+//
+//	public String getFirstName() {
+//		return firstName;
+//	}
+//
+//	public String getLastName() {
+//		return lastName;
+//	}
+//
+//	public boolean getStudentProblematicStatus() {
+//		return isProblematic;
+//	}
+}
