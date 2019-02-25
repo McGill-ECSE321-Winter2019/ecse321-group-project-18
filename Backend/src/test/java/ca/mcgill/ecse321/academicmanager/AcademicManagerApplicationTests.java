d854fab99836d408c7e2edfb0eb892971b9c5f87
Moetassem Abdelazim <moetassem.abdelazim@mail.mcgill.ca>
2019-02-25 09:16:58 -0500

    Added tests for CoopTermRegistration DAO and refactored the test code a bit #33

--- Backend/src/test/java/ca/mcgill/ecse321/academicmanager/AcademicManagerApplicationTests.java
+++ Backend/src/test/java/ca/mcgill/ecse321/academicmanager/AcademicManagerApplicationTests.java
@@ -6,25 +6,14 @@ import org.junit.runner.RunWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.springframework.boot.test.context.SpringBootTest;
-import ca.mcgill.ecse321.academicmanager.dao.CooperatorRepository;
-import ca.mcgill.ecse321.academicmanager.dao.CoopTermRegistrationRepository;
-import ca.mcgill.ecse321.academicmanager.dao.CourseRepository;
-import ca.mcgill.ecse321.academicmanager.dao.FormRepository;
-import ca.mcgill.ecse321.academicmanager.dao.MeetingRepository;
-import ca.mcgill.ecse321.academicmanager.dao.StudentRepository;
-import ca.mcgill.ecse321.academicmanager.dao.TermRepository;
+import ca.mcgill.ecse321.academicmanager.dao.*;	
 
 import ca.mcgill.ecse321.academicmanager.service.*;
 
 import org.mockito.invocation.InvocationOnMock;
 import org.springframework.test.context.junit4.SpringRunner;
 import ca.mcgill.ecse321.academicmanager.controller.AcademicManagerRestController;
-import ca.mcgill.ecse321.academicmanager.model.Student;
-import org.junit.Test;
-
-import org.junit.runner.RunWith;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit4.SpringRunner;
+import ca.mcgill.ecse321.academicmanager.model.*;
 
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
@@ -40,10 +29,13 @@ import static org.mockito.Mockito.when;
 
 public class AcademicManagerApplicationTests {
 
+	/********** STUDENT TEST *********/
 	@Mock
 		private StudentRepository studentDao;
 		@Mock
-			private TermRepository termDao;
+		private TermRepository termDao;
+		@Mock
+		private CoopTermRegistrationRepository coopTermRegistrationDao;
 		@Mock
 		private AcademicManagerService serviceMock;
 		@InjectMocks
@@ -53,11 +45,10 @@ public class AcademicManagerApplicationTests {
 
 		private Student student;
 		private static final String STUDENT_KEY = "226333211";
-		private static final String NONEXISTING_KEY = "NotaID";
-		
+		private static final String NONEXISTING_STUDENT_KEY = "NotaID";
 		
 		@Before
-		public void setMockOutput() {
+		public void setStudentMockOutput() {
 		  when(studentDao.findByStudentID(anyString())).thenAnswer( (InvocationOnMock invocation) -> {
 		    if(invocation.getArgument(0).equals(STUDENT_KEY)) {
 		      Student student = new Student();
@@ -70,7 +61,7 @@ public class AcademicManagerApplicationTests {
 		}
 		
 		@Before
-		public void setupMock() {
+		public void setupStudentMock() {
 			student = mock(Student.class);
 		}
 
@@ -80,13 +71,45 @@ public class AcademicManagerApplicationTests {
 		}
 
 		@Test
-		public void testParticipantQueryFound() {
-			Student mystudent=service.getStudent(STUDENT_KEY);
-		    assertEquals(STUDENT_KEY, mystudent.getStudentID());
+		public void testStudentQueryFound() {
+			assertEquals(STUDENT_KEY, service.getStudent(STUDENT_KEY).getStudentID());
 		}
 		
 	
-	
+	/**************** CoopTermRegistration TEST ***************/
+	@Mock	
+		private CoopTermRegistration coopTermRegistration;
+		private static final String COOPTERMREGISTRATION_KEY = "Internship001";
+		private static final String NONEXISTING_COOPREGISTRATION_KEY = "NotInternship";
+		
+		@Before
+		public void setCoopTermRegistrationMockOutput() {
+		when(coopTermRegistrationDao.findByRegistrationID(anyString())).thenAnswer( (InvocationOnMock invocation) -> {
+		    if(invocation.getArgument(0).equals(COOPTERMREGISTRATION_KEY)) {
+		      CoopTermRegistration coopTermRegistration = new CoopTermRegistration();
+		      coopTermRegistration.setRegistrationID(COOPTERMREGISTRATION_KEY);
+		      return coopTermRegistration;
+		    } else {
+		      return null;
+		    }
+		  });
+		}
+		
+		@Before
+		public void setupCoopTermRegistrationMock() {
+			coopTermRegistration = mock(CoopTermRegistration.class);
+		}
+
+		@Test
+		public void testMockCoopTermRegistrationCreation() {
+			assertNotNull(coopTermRegistration);
+		}
+
+		@Test
+		public void testCoopTermRegistrationQueryFound() {
+			assertEquals(COOPTERMREGISTRATION_KEY, service.getCoopTermRegistration(COOPTERMREGISTRATION_KEY).getRegistrationID());
+		}
+
 	@Test
 	public void contextLoads() {
 	}
