ac41d2e7ac16b1213623877334de035cb377b902
Bach <batr.schip@gmail.com>
2019-02-28 22:10:49 -0500

    added rest-assured for Integration Testing

---
+++ Backend/src/test/java/ca/mcgill/ecse321/academicmanager/controller/TestStudentRestController.java
@@ -0,0 +1,28 @@
+package ca.mcgill.ecse321.academicmanager.controller;
+
+
+import org.junit.Test;
+import org.junit.jupiter.api.BeforeAll;
+
+import static io.restassured.RestAssured.get;
+import static io.restassured.RestAssured.given;
+
+public class TestStudentRestController extends TestAcademicManagerRestController {
+    private String relation_name = "students/";
+    protected String _prefix = HOMEPAGE + relation_name;
+
+    @BeforeAll
+    public void PostCooperator() {
+        get(HOMEPAGE + "cooperators/create?id=1").then().assertThat().statusCode(OK);
+    }
+
+    @Test
+    public void TestView() {
+        given().
+                when().
+                get(_prefix).
+                then().
+                assertThat().
+                statusCode(OK);
+    }
+}
