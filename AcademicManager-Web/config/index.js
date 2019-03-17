01f3cdf07f8817a91132b99340c2e3e8145e31fd
yenvih <yen-vi.huynh@mail.mcgill.ca>
2019-03-17 23:31:46 +0000

    test deploy herokku #66

--- AcademicManager-Web/config/index.js
+++ AcademicManager-Web/config/index.js
@@ -23,7 +23,10 @@ module.exports = {
     // View the bundle analyzer report after build finishes:
     // `npm run build --report`
     // Set to `true` or `false` to always turn it on or off
-    bundleAnalyzerReport: process.env.npm_config_report
+    bundleAnalyzerReport: process.env.npm_config_report,
+    devServer: {
+      historyApiFallback: true,
+    }
   },
   dev: {
     env: require('./dev.env'),
