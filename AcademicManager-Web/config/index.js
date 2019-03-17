2e33a3d3de409b4b671018adcd7216043ea6e718
yenvih <yen-vi.huynh@mail.mcgill.ca>
2019-03-17 23:18:48 +0000

    test deploy herokku #66

--- AcademicManager-Web/config/index.js
+++ AcademicManager-Web/config/index.js
@@ -4,7 +4,7 @@ var path = require('path')
 module.exports = {
   build: {
     env: require('./prod.env'),
-    index: path.resolve(__dirname, '../dist/index.html'),
+    index: path.resolve(__dirname, 'index.html'),
     host: 'cooperatorapp-frontend-18.herokuapp.com',
     port: 443,
     backendHost: 'cooperatorapp-backend-18.herokuapp.com',
