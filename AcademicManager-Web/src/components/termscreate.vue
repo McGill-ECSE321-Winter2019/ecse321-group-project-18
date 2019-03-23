98b08cca68069787ecd9421c14fb5d08222403dc
yenvih <yen-vi.huynh@mail.mcgill.ca>
2019-03-23 04:45:15 +0100

    test integration

---
+++ AcademicManager-Web/src/components/termscreate.vue
@@ -0,0 +1,77 @@
+<template>
+<div id=termscreate>
+  <h2>Term</h2>
+  <input type="text" v-model="postID" placeholder="TermID"/>
+  <input type="text" v-model="postName" placeholder="TermName"/>
+  <input type="text" v-model="postSdline" placeholder="StudentDeadline"/>
+  <input type="text" v-model="postCdline" placeholder="CoopDeadline"/>
+  <button @click="createTerm(postID,postName,postSdline,postCdline)">Create Term</button>
+  <ul v-if="posts && posts.length">
+    <li v-for="post of posts" v-bind:key="post.id">
+      <p><strong>{{post.title}}</strong></p>
+      <p>{{post.body}}</p>
+    </li>
+  </ul>
+  
+  <ul>
+  <li v-if="errorTerm" style="color:red">Error: {{errorTerm}} </li>
+  <li v-else-if="newTerm"" style="color:red">Success: {{thisTerm}} </li>
+  </ul>
+</div>
+</template>
+
+<script>
+import axios from 'axios'
+var config = require('../../config')
+
+var frontendUrl = 'http://' + config.dev.host + ':' + config.dev.port
+var backendUrl = 'http://' + config.dev.backendHost + ':' + config.dev.backendPort
+
+
+var AXIOS = axios.create({
+  baseURL: backendUrl,
+  headers: { 'Access-Control-Allow-Origin': frontendUrl }
+})
+
+export default {
+  name: 'termscreate',
+  data () {
+    return {
+      thisTerm: [],
+      newTerm: '',
+      errorTerm: '',
+      response: []
+    }
+  },
+  methods: {
+    createTerm: function (postID,postName,postSdline,postCdline) {
+       AXIOS.post('/terms/create/?id=' + postID + '&name=' + postName + '&studentdeadline=' + postSdline + '&coopdeadline=' + postCdline,{},{}) 
+       
+	  .then(response => {
+	    // JSON responses are automatically parsed.
+	    this.thisTerm=[]
+	    this.thisTerm.push(response.data)
+	    this.newTerm = 'ok'
+	    this.errorTerm = ''
+	  })
+	  .catch(e => {
+	    var errorMsg = e.message
+	    console.log(errorMsg)
+	    this.errorTerm = errorMsg
+	  });
+	}
+  }
+}
+
+</script>
+
+
+<style>
+  #termscreate {
+    font-family: 'Avenir', Helvetica, Arial, sans-serif;
+    color: #2c3e50;
+    background: #f2ece8;
+  }
+</style>
+
+
--- AcademicManager-Web/src/router/index.js
+++ AcademicManager-Web/src/router/index.js
@@ -1,6 +1,9 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import Hello from '@/components/Hello'
+import termscreate from '@/components/termscreate'
+import studentslist from '@/components/studentslist'
+
 
 Vue.use(Router)
 
@@ -10,6 +13,16 @@ export default new Router({
       path: '/',
       name: 'Hello',
       component: Hello
+    },
+    {
+      path: '/terms/create',
+      name: 'termscreate',
+      component: termscreate
+    },
+    {
+      path: '/students/list',
+      name: 'studentslist',
+      component: studentslist
     }
   ]
 })
