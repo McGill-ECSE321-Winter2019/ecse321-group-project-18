21af456ef4a903de7f8dcf5f835c12aec4e62a1a
sbakhit <saleh.bakhit@hotmail.com>
2019-03-22 20:32:50 -0400

    student vue incomplete

--- AcademicManager-Web/src/components/students.vue
+++ AcademicManager-Web/src/components/students.vue
@@ -1,54 +1,104 @@
 <template>
   <div class="page-content">
-	  <h2 class="pagetitle">Students</h2>
+    <h2 class="pagetitle">Students</h2>
+    <div class="filters-section">
+      <h4 class="search-title">Search</h4>
+      <div class ="filters-entries">
+        <input type="text" name="name" placeholder="Student ID">
+        <input type="text" name="lastname" placeholder="Student name">
+        <select>
+          <option value="all">All Students</option>
+          <option value="problematic">Problematic</option>
+        </select>
+        <button>Search</button>
+      </div>
+    </div>
 
-	  <div class="filters-section">
-	    <h4 class="search-title">Search</h4>
-	    <div class ="filters-entries">
-	      <input type="text" name="name" placeholder="Student ID">
-	      <input type="text" name="lastname" placeholder="Student name">
-	      <select>
-	        <option value="all">All Students</option>
-	        <option value="problematic">Problematic</option>
-	      </select>
-	      <button>Search</button>
-	    </div>
-	  </div>
+    <b-table striped bordered hover :items="students" :fields="fields" :caption-top="true" :busy="isBusy">
+
+    </b-table>
   </div>
 </template>
 
 
 <script>
+  import axios from 'axios'
+
+  var config = require('../../config')
+
+  var frontendUrl = 'http://' + config.dev.host + ':' + config.dev.port
+  var backendUrl = 'http://' + config.dev.backendHost + ':' + config.dev.backendPort
+
+  var AXIOS = axios.create({
+    baseURL: backendUrl,
+    headers: { 'Access-Control-Allow-Origin': frontendUrl }
+  })
+
+  function studentDto(studentid, first_name, last_name, is_problematic) {
+    this.studentid = studentid
+    this.first_name = first_name
+    this.last_name = last_name
+    this.is_problematic = is_problematic
+  }
+
+  export default {
+    name: 'students',
+    data() {
+      return {
+        students: [],
+        newStudent: '',
+        errorStudent: '',
+        response: []
+      }
+    },
+    created: function () {
+      AXIOS.get('/students/list')
+        .then(response => {
+          this.students = response.data
+        })
+        .catch(e => {
+          this.errorStudent = e
+        });
+    },
+    methods: {
+      createStudent: function(studentid, first_name, last_name, is_problematic) {
+        AXIOS.post('/students/create/?id=${studentid}&firstname=${first_name}&lastname=${last_name}&cooperatorid=1')
+        const s = new studentDto(studentid, first_name, last_name, is_problematic)
+        this.students.push(s)
+
+        this.newStudent = ''
+      }
+    }
+  }
 </script>
 
 
-<style>
-.filters-entries input{
-	width: 30%;
-	padding-left: 5px;
-	border: 1px solid;
-	border-radius: 4px;
-}
+<style scoped>
+  .filters-entries input{
+    width: 30%;
+    padding-left: 5px;
+    border: 1px solid;
+    border-radius: 4px;
+  }
 
-.filters-entries select{
-	width: 20%;
-	height: 100%;
-	padding: 3.5px 5px;
-	border: 1px solid;
-	border-radius: 4px;
-}
+  .filters-entries select{
+    width: 20%;
+    height: 100%;
+    padding: 3.5px 5px;
+    border: 1px solid;
+    border-radius: 4px;
+  }
 
-.filters-entries button{
-	margin-left: 5%;
-	background-color: #dc241f;
-	border: 1px solid #dc241f;
-	color: white;
-	padding: 5px 20px;
-	border-radius: 4px;
-}
+  .filters-entries button{
+    margin-left: 5%;
+    background-color: #dc241f;
+    border: 1px solid #dc241f;
+    color: white;
+    padding: 5px 20px;
+    border-radius: 4px;
+  }
 
-.filters-entries button:hover{
-	background-color: #8b0c00;
-}
-
+  .filters-entries button:hover{
+    background-color: #8b0c00;
+  }
 </style>
