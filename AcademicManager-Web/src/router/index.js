f8bcc52b854c5f37f88f22f98a802ffd77145ff6
Moetassem Abdelazim <moetassem.abdelazim@mail.mcgill.ca>
2019-03-24 20:11:37 -0400

    Deleted Login but kept files

--- AcademicManager-Web/src/router/index.js
+++ AcademicManager-Web/src/router/index.js
@@ -1,7 +1,6 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import Hello from '@/components/Hello'
-import Login from '@/components/Login'
 import internships from '@/components/internships'
 import students from '@/components/students'
 import courses from '@/components/courses'
@@ -12,14 +11,8 @@ export default new Router({
   routes: [
     {
       path: '/',
-      redirect: '/login',
       name: 'Hello',
       component: Hello
-    },
-    {
-      path: '/login',
-      name: 'Login',
-      component: Login
     },
     {
       path: '/internships',
