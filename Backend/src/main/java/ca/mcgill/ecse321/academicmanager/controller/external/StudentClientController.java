5275ca451219024e6a4be61abe31c71716f14d1b
Bach <batr.schip@gmail.com>
2019-03-31 19:23:24 -0400

    added gson and succesfully fetched necessary data from the Student team

--- Backend/src/main/java/ca/mcgill/ecse321/academicmanager/controller/external/StudentClientController.java
+++ Backend/src/main/java/ca/mcgill/ecse321/academicmanager/controller/external/StudentClientController.java
@@ -1,20 +1,48 @@
 package ca.mcgill.ecse321.academicmanager.controller.external;
 
 import ca.mcgill.ecse321.academicmanager.service.StudentService;
+import com.google.gson.*;
 import org.springframework.web.bind.annotation.*;
 
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.OutputStream;
+import java.lang.reflect.Type;
 import java.net.HttpURLConnection;
 import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
+
+
+
+/**
+ * This class is an intermediate class capturing the JSON object on the Student team.
+ * @author Bach Tran
+ * */
+class ExternalStudentDto {
+    protected String name;
+    protected String studentID;
+//    protected String email;
+//    protected String major;
+//    protected String phone;
+//    protected String coopPosition;
+
+    public ExternalStudentDto(String name, String studentID) {
+        this.name = name;
+        this.studentID = studentID;
+    }
+
+    @Override
+    public String toString() {
+        return "ID = " + studentID + " name = " + name;
+    }
+}
 
 @RestController
 public class StudentClientController {
 
     public static final String GET_URL = "https://employer-backend-8888.herokuapp.com/mainapp/1/getstudents";
-
     private StudentService studentService;
     /**
      * source: https://www.journaldev.com/7148/java-httpurlconnection-example-java-http-request-get-post
@@ -42,19 +70,40 @@ public class StudentClientController {
 
             responseString = response.toString();
         }
-        System.out.println(responseString + responseCode);
         return responseString;
     }
-
+    /**
+     * Response method for the HTTP GET request /students/sync
+     * @return a message to the REST Client
+     * @author Bach Tran
+     * */
     @GetMapping(value = { "/students/sync", "/students/sync/" })
     public String getAllStudents() {
         String responseMessage = "Sync complete!";
+        String responseString = "";
         try {
-            sendGETStudents();
+            responseString = sendGETStudents();
         } catch (IOException e) {
             System.out.println(e.getMessage());
             responseMessage = e.getMessage();
         }
-        return responseMessage;
+
+       Gson gson = new Gson();
+//        Type type = new TypeToken<ExternalStudentDto[]>(){}.getType();
+//        ExternalStudentDto[] externalStudentDto = gson.fromJson(responseString, ExternalStudentDto[].class);
+        // parse raw data
+        JsonParser parser = new JsonParser();
+        JsonArray jsonStudents = parser.parse(responseString).getAsJsonArray();
+        // parse to Java Objects
+        List<ExternalStudentDto> students = new ArrayList<>();
+        for (JsonElement jsonStudent : jsonStudents) {
+            if (jsonStudent.isJsonObject()) {
+                JsonObject intermediateObject = jsonStudent.getAsJsonObject();
+                students.add(new ExternalStudentDto(intermediateObject.get("studentID").toString(),
+                        intermediateObject.get("name").toString()));
+            }
+            System.out.println(students);
+        }
+        return responseMessage + "\n" + students.toString();
     }
 }
