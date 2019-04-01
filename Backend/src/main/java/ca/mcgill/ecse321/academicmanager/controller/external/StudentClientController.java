20524b113f1a132f9e6d2ab57ce7b93ba07bd07c
Bach <batr.schip@gmail.com>
2019-04-01 15:19:53 -0400

    moved nameSeparator to ExtendedStudentDto class & added comments

--- Backend/src/main/java/ca/mcgill/ecse321/academicmanager/controller/external/StudentClientController.java
+++ Backend/src/main/java/ca/mcgill/ecse321/academicmanager/controller/external/StudentClientController.java
@@ -13,32 +13,76 @@ import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.URL;
 import java.util.HashMap;
-import java.util.Set;
 
 
 /**
- * This class is an intermediate class capturing the JSON object on the Student team.
+ * An intermediate class capturing the JSON object on the Student team.
+ * This class is in charge of making the data in the Student team and the AcademicManger team compatible.
  * @author Bach Tran
  * */
 class ExternalStudentDto {
-    protected String name;
+    protected String fullName;
     protected String studentID;
+    protected String firstName;
+    protected String lastName;
 //    protected String email;
 //    protected String major;
 //    protected String phone;
 //    protected String coopPosition;
-
-    public ExternalStudentDto(String studentID, String name) {
-        this.name = name;
+    /**
+     * Creates a new ExternalStudentDto object, this object captures relevant data on the Student team.
+     * @param fullName the full name of the Student. This will be automatically split into firstName and lastName
+     * @param studentID the unique id of the Student.
+     *
+     */
+    public ExternalStudentDto(String studentID, String fullName) {
+        this.fullName = fullName;
         this.studentID = studentID;
+        this.firstName = nameSeparator(this.fullName)[0];
+        this.lastName = nameSeparator(this.fullName)[1];
     }
 
     @Override
     public String toString() {
-        return "ID = " + studentID + " name = " + name;
+        return "ID = " + studentID + " fullName = " + fullName;
+    }
+
+    /**
+     * Background: the fullName data on the Student team has only a single field, attaching both the first fullName and
+     * the last fullName of the student. While on the Academic Manger's side, we separate those two attributes.
+     * This method provides an adaptation to this difference.
+     * @param name the raw fullName
+     * @return a String array contains exactly two elements: the first fullName and the last fullName.
+     * */
+    private static String[] nameSeparator(String name) {
+        String[] splitted = name.split(" ");
+        // assumes the student doesn't have a last name.
+        if (splitted.length < 2) {
+            String first_name = splitted[0];
+            String last_name = "<No_last_name>";
+            splitted = new String[2];
+            splitted[0] = first_name;
+            splitted[1] = last_name;
+        }
+        // name has more than three words, in this case, the first word will be the first name,
+        // the rest will become the last name.
+        else if (splitted.length > 2) {
+            String first_name = splitted[0];
+            String last_name = "";
+            for (int i = 1; i < splitted.length; i++) {
+                last_name += splitted[i];
+            }
+            splitted = new String[2];
+            splitted[0] = first_name;
+            splitted[1] = last_name;
+        }
+        return splitted;
     }
 }
-
+/**
+ * Main class handle the synchronization with the Student team's data.
+ * @author Bach Tran
+ * */
 @RestController
 public class StudentClientController {
     public static final String GET_URL = "https://employer-backend-8888.herokuapp.com/mainapp/1/getstudents";
@@ -48,6 +92,7 @@ public class StudentClientController {
     @Autowired
     private CooperatorService cooperatorService;
     /**
+     * Low-level method to open a connection to the Student team's RESTful calls.
      * source: https://www.journaldev.com/7148/java-httpurlconnection-example-java-http-request-get-post
      * @author Bach Tran
      * @return a String in JSON format, or an empty String if request has an error.
@@ -75,7 +120,8 @@ public class StudentClientController {
         return responseString;
     }
     /**
-     * Main response method for the HTTP GET request /students/sync
+     * Main response method for the HTTP GET request /students/sync.
+     * This method aims to get all Student from the Student team's database.
      * @return a message to the REST Client
      * @author Bach Tran
      * */
@@ -119,34 +165,6 @@ public class StudentClientController {
         return result;
     }
     /**
-     * Background: the name data on the Student team has only a single field, attaching both the first name and
-     * the last name of the student. While on the Academic Manger's side, we separate those two attributes.
-     * This method provides an adaptation to this difference.
-     * @param name the raw name
-     * @return a String array contains exactly two elements: the first name and the last name.
-     * */
-    private static String[] nameSeperator(String name) {
-        String[] splitted = name.split(" ");
-        if (splitted.length < 2) {
-            String first_name = splitted[0];
-            String last_name = "<No_last_name>";
-            splitted = new String[2];
-            splitted[0] = first_name;
-            splitted[1] = last_name;
-        } else if (splitted.length > 2) {
-            String first_name = splitted[0];
-            String last_name = "";
-            for (int i = 1; i < splitted.length; i++) {
-                last_name += splitted[i];
-            }
-            splitted = new String[2];
-            splitted[0] = first_name;
-            splitted[1] = last_name;
-        }
-        return splitted;
-    }
-
-    /**
      * Puts the received data into the Backend's database.
      * Contains the logic (instruction) on how to appropriately inject data to the database.
      * After this method, the database of the AcademicManager will be the same to that of the Student team.
@@ -170,16 +188,17 @@ public class StudentClientController {
             if (studentService.exists(externalStudentID)) {
                 // updates the existing student
                 Student student = studentService.get(externalStudentID);
-                studentService.updateFirstName(student ,nameSeperator(students.get(externalStudentID).name)[0]);
-                studentService.updateLastName(student, nameSeperator(students.get(externalStudentID).name)[1]);
+                studentService.updateFirstName(student, students.get(externalStudentID).firstName);
+                studentService.updateLastName(student, students.get(externalStudentID).lastName);
             } else {
                 // create new, non-problematic student
                 studentService.create(externalStudentID,
-                        nameSeperator(students.get(externalStudentID).name)[0],
-                        nameSeperator(students.get(externalStudentID).name)[1],
+                        students.get(externalStudentID).firstName,
+                        students.get(externalStudentID).lastName,
                         cooperatorService.get(DEFAULT_COOPERATOR_ID));
             }
         }
+        // optional: return message to console.
         System.out.println("Updated data in the backend database!");
     }
 }
